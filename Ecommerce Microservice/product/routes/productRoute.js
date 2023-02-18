891b2193682a4ec1f4ce300a4c7064b9032fb2f5
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-02-19 04:13:56 +0530

    init

--- Ecommerce Microservice/product/routes/productRoute.js
+++ Ecommerce Microservice/product/routes/productRoute.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const { buy,create }= require('../controllers/productController');
-const { isAuthenticated }= require('../../middleware/isAuthenticated');
+const { buy, create } = require("../controllers/productController");
+const { isAuthenticated } = require("../middleware/isAuthenticated");
 
-Router.post("/product/create",isAuthenticated,create);
-Router.post("/product/buy",isAuthenticated,buy);
+Router.post("/product/create", isAuthenticated, create);
+Router.post("/product/buy", isAuthenticated, buy);
 
 module.exports = Router;
